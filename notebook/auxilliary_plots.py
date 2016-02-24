bf27ffc4a086c08a7e895badb46e7abf24ad7396
philipp.s.lang@gmail.com <philipp.s.lang@gmail.com>
2016-02-24 10:42:48 +0000

    roots notebook incl solutions final

--- notebook/auxilliary_plots.py
+++ notebook/auxilliary_plots.py
@@ -12,7 +12,7 @@ matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams['font.sans-serif'] = ['Arial']
 
 
-def picard_convergence_pattern(fct, x, max_labels=6, tol=1.0E-4):
+def picard_convergence_pattern(fct, x, max_labels=6, tol=1.0E-4, flabel=''):
     fig, ax = plt.subplots(figsize=(8,6))
 
     # solve
@@ -51,16 +51,20 @@ def picard_convergence_pattern(fct, x, max_labels=6, tol=1.0E-4):
         label = ''.join(['$x_', str(i), '$'])
         ax.text(h_p[i]+dxt, fp_p[i]+dyt, label)
     
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$g(x)$')
-    plt.title('Successive Approximations')
+    ax.set_xlabel('$x$', fontsize=16)
+    if not flabel:
+        fl = '$f(x)$'
+    else:
+        fl = flabel
+    ax.set_ylabel(fl, fontsize=16)
+    plt.title('Successive Approximations', fontsize=20)
     plt.show()
 
 
-def root_bracketing_pattern(f, a, b, dx, xbounds=(-0.1,1.4), ybounds=(-5,6)):
+def root_bracketing_pattern(f, a, b, dx, xbounds=(-0.1,1.4), ybounds=(-5,6), flabel=''):
     x = np.linspace(a, b, int((b-a)/dx)+1)
     y = f(x)
-    fig, ax = plt.subplots(figsize=(16,6))
+    fig, ax = plt.subplots(figsize=(14,6))
     ax.plot(x, y, marker='x', color='r')
     switched = False
     for i, xt in enumerate(x):
@@ -70,8 +74,12 @@ def root_bracketing_pattern(f, a, b, dx, xbounds=(-0.1,1.4), ybounds=(-5,6)):
             if not switched and np.sign(y[i]) != np.sign(y[i-1]):
                 pass
                 ax.plot([x[i],x[i-1]], [y[i],y[i-1]], color='b')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$f(x)$')
+    ax.set_xlabel('$x$', fontsize=16)
+    if not flabel:
+        fl = '$f(x)$'
+    else:
+        fl = flabel
+    ax.set_ylabel(fl, fontsize=16)
     ax.plot([a,b],[0.,0.],color='gray',ls='-.',alpha=0.75)
     ax.set_xlim(a,b)
     ax.set_ylim(-4,1)
@@ -79,7 +87,7 @@ def root_bracketing_pattern(f, a, b, dx, xbounds=(-0.1,1.4), ybounds=(-5,6)):
     plt.show()
 
 
-def bisection_pattern(f, x1, x2, tol=1.0e-5, inset=True, ixmin=0.4, ixmax=0.7, iymin=-0.25, iymax=0.2, zoom=5, loc0=4, loc1=2, loc2=1):
+def bisection_pattern(f, x1, x2, tol=1.0e-5, inset=True, ixmin=0.4, ixmax=0.7, iymin=-0.25, iymax=0.2, zoom=5, loc0=4, loc1=2, loc2=1, flabel=''):
     fig, ax = plt.subplots(figsize=(10,6))
     plt.title('Bisection', fontsize=20)
     x = np.linspace(x1, x2, 100)
@@ -136,17 +144,20 @@ def bisection_pattern(f, x1, x2, tol=1.0e-5, inset=True, ixmin=0.4, ixmax=0.7, i
         mark_inset(ax, axins, loc1=loc1, loc2=loc2, fc="none", ec="0.5")
         
     xf = (x1 + x2)/2.0
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$f(x)$')
+    ax.set_xlabel('$x$', fontsize=16)
+    if not flabel:
+        fl = '$f(x)$'
+    else:
+        fl = flabel
+    ax.set_ylabel(fl, fontsize=16)
     plt.show()
 
 
-def newton_convergence_pattern(fn, dx, x_0, tol, max_labels0=1, max_labels1=2, inset=True, ixmin=3.0, ixmax=3.2, iymin=-0.1, iymax=0.1, zoom=8, loc0=1, loc1=3, loc2=2, maxiter=100):
+def newton_convergence_pattern(fn, dx, x_0, tol, max_labels0=1, max_labels1=2, inset=True, ixmin=3.0, ixmax=3.2, iymin=-0.1, iymax=0.1, zoom=8, loc0=1, loc1=3, loc2=2, maxiter=100, resfct=100, flabel=''):
     fig, ax = plt.subplots(figsize=(10,6))
     plt.title('Newton', fontsize=20)
     x_n = [x_0]
-    y_n = [fn(x_0)]
-    
+    y_n = [fn(x_0)]    
     fx_n = [x_0]
     fy_n = [fn(x_0)]
     
@@ -175,7 +186,7 @@ def newton_convergence_pattern(fn, dx, x_0, tol, max_labels0=1, max_labels1=2, i
     ax.plot(x_n, y_n, color='green', ls='--')
         
     # the function
-    x = np.linspace( np.min(x_n), np.max(x_n), 50)
+    x = np.linspace( np.min(x_n), np.max(x_n), resfct)
     f = fn(x)
     ax.plot(x, f, color='red', alpha=0.5)
     
@@ -200,8 +211,7 @@ def newton_convergence_pattern(fn, dx, x_0, tol, max_labels0=1, max_labels1=2, i
         axins.plot(x, f, color='red', alpha=0.5)
         for i in range(max_labels0+1, min(max_labels0+1+max_labels1,len(fx_n))):
             label = ''.join(['$x_', str(i), '$'])
-            axins.text(fx_n[i]+dxt/8., fy_n[i]+dyt/8., label)
-        
+            axins.text(fx_n[i]+dxt/8., fy_n[i]+dyt/8., label)        
         axins.plot([ixmin,ixmax],[0.,0.],color='gray',ls='-.',alpha=0.75)
         axins.set_xlim(ixmin, ixmax)
         axins.set_ylim(iymin, iymax)
@@ -209,7 +219,87 @@ def newton_convergence_pattern(fn, dx, x_0, tol, max_labels0=1, max_labels1=2, i
         axins.get_yaxis().set_visible(False)
         mark_inset(ax, axins, loc1=loc1, loc2=loc2, fc="none", ec="0.5")
     
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$f_n(x)$')
+    ax.set_xlabel('$x$', fontsize=16)
+    if not flabel:
+        fl = '$f(x)$'
+    else:
+        fl = flabel
+    ax.set_ylabel(fl, fontsize=16)
+    plt.show()
+
+
+def secant_convergence_pattern(fn, x_0, x_1, tol, max_labels0=1, max_labels1=2, inset=True, ixmin=3.0, ixmax=3.2, iymin=-0.1, iymax=0.1, zoom=8, loc0=1, loc1=3, loc2=2, maxiter=100, resfct=100, flabel=''):
+    fig, ax = plt.subplots(figsize=(10,6))
+    plt.title('Secant', fontsize=20)
+    x_n = [x_0, x_1] # conv pattern
+    y_n = [fn(x_0), fn(x_1)]    # conv pattern
+    fx_n = [x_0, x_1] # x_n
+    fy_n = [fn(x_0), fn(x_1)]# f(x_n)
+    
+    # solving
+    i = 0
+    while 1:
+        b = (fn(fx_n[-1]) - fn(fx_n[-2])) / (fx_n[-1]-fx_n[-2])        
+        a = fn(fx_n[-1])-b*fx_n[-1]
+        x_zero = -a/b
+        x_n.append(x_zero)        
+        y_n.append(0.)
+        
+        fx_n.append(x_zero)
+        fy_n.append(fn(x_zero))
+        if abs(x_n[-1]-x_n[-2]) < tol:
+            break
+        x_n.append(x_zero)
+        y_n.append(fn(x_zero))
+        i = i+1
+        if i >= maxiter:
+            break
+        
+    # the iteration results
+    ax.scatter(fx_n, fy_n, marker='x', color='blue', s=30)
+    
+    # the convergence pattern
+    ax.plot(x_n, y_n, color='green', ls='--')
+     
+    # the function
+    x = np.linspace( np.min(x_n), np.max(x_n), resfct)
+    f = fn(x)
+    ax.plot(x, f, color='red', alpha=0.5)
+    
+    # initial guess
+    dxt = (np.max(x_n)-np.min(x_n))/35.
+    dyt = (np.max(y_n)-np.min(y_n))/35.
+    ax.text(fx_n[0]+dxt, fy_n[0]+dyt, '$x_0$')    
+    for i in range(1, min(max_labels0+1, len(fx_n))):
+        label = ''.join(['$x_', str(i), '$'])
+        ax.text(fx_n[i]+dxt, fy_n[i]+dyt, label)
+
+    # zero line
+    xlim = ax.get_xlim()
+    ax.plot([xlim[0],xlim[1]],[0.,0.],color='gray',ls='-.',alpha=0.75)
+    ax.set_xlim(xlim)
+
+    # zoomed inset
+    if inset:
+        axins = zoomed_inset_axes(ax, zoom, loc=loc0)
+        axins.scatter(fx_n, fy_n, marker='x', color='blue', s=30)
+        axins.plot(x_n, y_n, color='green', ls='--')
+        axins.plot(x, f, color='red', alpha=0.5)
+        for i in range(max_labels0+1, min(max_labels0+1+max_labels1,len(fx_n))):
+            label = ''.join(['$x_', str(i), '$'])
+            axins.text(fx_n[i]+dxt/8., fy_n[i]+dyt/8., label)        
+        axins.plot([ixmin,ixmax],[0.,0.],color='gray',ls='-.',alpha=0.75)
+        axins.set_xlim(ixmin, ixmax)
+        axins.set_ylim(iymin, iymax)
+        axins.get_xaxis().set_visible(False)
+        axins.get_yaxis().set_visible(False)
+        mark_inset(ax, axins, loc1=loc1, loc2=loc2, fc="none", ec="0.5")
+    
+    ax.set_xlabel('$x$', fontsize=16)
+    if not flabel:
+        fl = '$f(x)$'
+    else:
+        fl = flabel
+    ax.set_ylabel(fl, fontsize=16)
     plt.show()
  
